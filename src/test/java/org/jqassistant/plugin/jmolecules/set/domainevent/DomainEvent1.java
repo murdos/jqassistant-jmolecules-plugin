9ad5812c8540ab9efed112620699eb0a28ce3ae6
Stephan Pirnbaum <stephan.pirnbaum@googlemail.com>
2023-07-21 19:49:29 +0200

    #9 support externalized events

--- src/test/java/org/jqassistant/plugin/jmolecules/set/domainevent/DomainEvent1.java
+++ src/test/java/org/jqassistant/plugin/jmolecules/set/domainevent/DomainEvent1.java
@@ -1,7 +1,9 @@
 package org.jqassistant.plugin.jmolecules.set.domainevent;
 
 import org.jmolecules.event.annotation.DomainEvent;
+import org.jmolecules.event.annotation.Externalized;
 
+@Externalized
 @DomainEvent
 public class DomainEvent1 {
 }
--- src/test/java/org/jqassistant/plugin/jmolecules/set/domainevent/DomainEvent2.java
+++ src/test/java/org/jqassistant/plugin/jmolecules/set/domainevent/DomainEvent2.java
@@ -1,6 +1,7 @@
 package org.jqassistant.plugin.jmolecules.set.domainevent;
 
 import org.jmolecules.event.types.DomainEvent;
+import org.jmolecules.event.types.Externalized;
 
-public class DomainEvent2 implements DomainEvent {
+public class DomainEvent2 implements DomainEvent, Externalized {
 }
