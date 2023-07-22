dfa0a58ad62eb7a0a1ef210c40ab16efe5a6af3c
Aurélien Mino <aurelien.mino@gmail.com>
2023-07-22 13:56:15 +0200

    Don't create self relationship DEPENDS_ON for BoundedContextPackage

--- src/test/java/org/jqassistant/plugin/jmolecules/set/bc/bc2/OrderService.java
+++ src/test/java/org/jqassistant/plugin/jmolecules/set/bc/bc2/OrderService.java
@@ -8,6 +8,8 @@ import java.util.List;
 @Service
 public class OrderService {
 
+    private OrderRepository orderRepository;
+
     public void createOrderForProducts(List<Product> products) {
 
     }
